ee376992cea319181b0a63f6d3237230c55ba9d6
Andrii Serha <sergaav@gmail.com>
2021-04-12 15:12:18 +0300

    Delete Util

--- src/main/java/com/epam/rd/java/basic/practice5/Part4.java
+++ src/main/java/com/epam/rd/java/basic/practice5/Part4.java
@@ -1,7 +1,6 @@
 package com.epam.rd.java.basic.practice5;
 
 import java.io.*;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
